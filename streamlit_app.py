eb8decb6285a263efb2ca5aaff0a3fb5fa665f43
Lito Valdez <66933650+Litius333@users.noreply.github.com>
2025-05-27 15:27:38 +0000

    graficos

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+df = sns.load_dataset('tips')
+
+select_sex = st.selectbox('Elegir:', df.sex.unique())
+
+df.loc[df.sex == select_sex]
+
+g = sns.displot(data = df.loc[df.sex == select_sex], x = 'total_bill',kind= 'kde')
+st.pyplot(g)
+
+f, ax = plt.subplots()
+ax = sns.barplot(data=df, x='sex', y='tip', hue='smoker')
+st.pyplot(f)
