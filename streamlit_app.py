a46f83c6e18614e7fbd2da05a93bbd0225d11058
Lito Valdez <66933650+Litius333@users.noreply.github.com>
2025-07-12 17:59:38 +0000

    Check

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-st.title("🎈 My new app")
+st.title("Proyecto : Protocolo Fantasma")
 
 df = sns.load_dataset('tips')
 
@@ -12,9 +12,14 @@ select_sex = st.selectbox('Elegir:', df.sex.unique())
 
 df.loc[df.sex == select_sex]
 
-g = sns.displot(data = df.loc[df.sex == select_sex], x = 'total_bill',kind= 'kde')
-st.pyplot(g)
 
-f, ax = plt.subplots()
-ax = sns.barplot(data=df, x='sex', y='tip', hue='smoker')
-st.pyplot(f)
+agree = st.checkbox("Generar grafico de densidad")
+if agree:
+    g = sns.displot(data = df.loc[df.sex == select_sex], x = 'total_bill',kind= 'kde')
+    st.pyplot(g)
+
+agree2 = st.checkbox("Generar grafico de barras")
+if agree2:
+    f, ax = plt.subplots()
+    ax = sns.barplot(data=df, x='sex', y='tip', hue='smoker')
+    st.pyplot(f)
