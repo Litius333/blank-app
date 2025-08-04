55d72b60265008cbf0f8266b6c605a5eba953240
Lito Valdez <66933650+Litius333@users.noreply.github.com>
2025-08-04 18:12:54 +0000

    agrupar

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,6 @@ select_sex = st.selectbox('Elegir:', df.sex.unique())
 
 df.loc[df.sex == select_sex]
 
-
 agree = st.checkbox("Generar grafico de densidad")
 if agree:
     g = sns.displot(data = df.loc[df.sex == select_sex], x = 'total_bill',kind= 'kde')
@@ -23,3 +22,16 @@ if agree2:
     f, ax = plt.subplots()
     ax = sns.barplot(data=df, x='sex', y='tip', hue='smoker')
     st.pyplot(f)
+
+agrupar = df.groupby(['sex', 'day'])[['total_bill']].mean()
+st.table(agrupar)
+
+gh, ax =plt.subplots()
+ax = sns.barplot(data= df, y = df['total_bill'], x = 'sex', hue='time')
+st.pyplot(gh)
+
+avg_bill_by_day = df.groupby(['day', 'sex'])['total_bill'].mean()
+st.table(avg_bill_by_day)
+
+avg_tip_by_time = df.groupby('time')['tip'].mean()
+st.table(avg_tip_by_time)
